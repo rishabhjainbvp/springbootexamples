e88ca58e8fb0984942093efeaa110148abb7d6c7
jairis <rishabhjainbvp@gmail.com>
2018-05-17 11:46:06 +0200

    Master Commit - First

---
+++ src/main/java/com/example/messaging/activemqspringbootexample/ActivemqSpringbootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.messaging.activemqspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqSpringbootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqSpringbootExampleApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/messaging/activemqspringbootexample/listener/Consumer.java
@@ -0,0 +1,14 @@
+package com.example.messaging.activemqspringbootexample.listener;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+	@JmsListener(destination="standalone.queue")
+	public void consume(String message) {
+		 System.out.println("Received message:: "+ message);
+	}
+	
+}
